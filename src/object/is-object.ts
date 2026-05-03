f09b0a185cdda669f6da65daf4b4516da362de3e
Wolfchamane <arturo.martinez@amartinez.dev>
2026-05-03 12:57:06 +0200

    feature (/src/object/is-object.ts): added assertion type
    [NO-REF]

--- src/object/is-object.ts
+++ src/object/is-object.ts
@@ -22,6 +22,6 @@ import type { Nil } from '../types';
  * @param   {any}       value To be evaluated
  * @return  {Boolean}   `true` if {value} is not `null`, `undefined` and `Array` or any other different to object.
  */
-export const isObject = (value: Nil<any>): boolean =>
+export const isObject = (value: Nil<any>): value is object =>
     value !== null && value !== undefined && typeof value === 'object' && !Array.isArray(value);
 /* eslint-enable @typescript-eslint/no-explicit-any */
